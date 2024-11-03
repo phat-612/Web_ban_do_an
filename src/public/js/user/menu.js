da1efe87f333ce857a285c70b536a5e16cb7362b
CamHung1609 <camdaica20003@gmail.com>
2024-11-03 23:17:53 +0700

    b

--- src/public/js/user/menu.js
+++ src/public/js/user/menu.js
@@ -1,4 +1,18 @@
 var minusAddmoreBtn = Document.querySelector("#button-minus-Addmore");
 var plusAddmoreBtn = Document.querySelector("#button-plus-Addmore");
 
-function SubAddmore() {}
+$("#button-minus-Addmore").on("click", function (e) {
+  const btn = e.target;
+  const inputGroup = $(btn.closest(".input-group"));
+  const inpValue = btn.getAtrribute("quantity-Addmore");
+});
+
+function sendFetchUpdateQuantityAddmore() {
+  fetch("", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  }).then((response) => {});
+}
