3fc37747555cfb1802accbf425840f5c3362d778
Nguyễn Văn Phát <82584665+phat-612@users.noreply.github.com>
2024-11-06 11:59:53 +0700

    done header

--- src/middlewares/globalVariables.js
+++ src/middlewares/globalVariables.js
@@ -2,10 +2,15 @@ import shopModel from "../services/ShopModel";
 
 const globalVariables = async (req, res, next) => {
   try {
-    // res.locals.userLogin = null;
-    // if (req.session.userLogin) {
-    //   res.locals.userLogin = req.session.user;
-    // }
+    res.locals.userLogin = null;
+    if (req.session.user) {
+      res.locals.userLogin = req.session.user;
+    }
+    console.log(
+      "globalVariables -> res.locals.userLogin",
+      res.locals.userLogin
+    );
+    console.log("globalVariables -> req.session.userLogin", req.session.user);
     const infoShop = await shopModel.getInfoShop();
     res.locals.infoShop = infoShop[0];
     next();
