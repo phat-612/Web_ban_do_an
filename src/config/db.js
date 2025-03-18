1a17514e364ff2315c305a99476e1338e533bf68
NguyenPhat <nguyenphatssj0612@gmail.com>
2025-03-18 17:31:57 +0700

    a

--- src/config/db.js
+++ src/config/db.js
@@ -8,15 +8,9 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  // port: process.env.DB_PORT,
+  port: process.env.DB_PORT,
 });
 
 const connection = pool.promise();
-// connection.query("SELECT 1", (err, results) => {
-//   if (err) {
-//     console.error("Database connection failed:", err.stack);
-//   } else {
-//     console.log("Database connection successful:", results);
-//   }
-// });
+
 export default connection;
